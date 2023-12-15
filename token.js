7e5b25cda04c863848236d47e966d6d9bdb85f4a
ZechArslan <zecharslan@gmail.com>
2023-12-15 17:01:17 +0500

    Add menu

--- token.js
+++ token.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
+  $(".toggle-menu").click(function () {
+    $(".sidebar").toggleClass("active");
+  });
   var datestart = "";
   var dateend = "";
   var Api;
+
   var dataRange = [false, true, true, true, true, false];
   $("#token_table thead tr")
     .clone(true)
     .addClass("filters")
     .appendTo("#token_table thead");
+
   var table = $("#token_table").DataTable({
     orderCellsTop: true,
     scrollX: true,
@@ -16,7 +21,6 @@ $(document).ready(function () {
     serverMethod: "post",
     oLanguage: { sProcessing: "<div id='loader'></div>" },
 
-    // ajax: '../../datatable.php', ///Path will change here
     ajax: {
       url: "http://144.76.163.123/dashboard/tokens.php?action=get_all_tokens",
 
@@ -38,28 +42,47 @@ $(document).ready(function () {
         window["lastpayload"] = d;
       },
     },
+
     columns: [
       { data: "Id" },
       { data: "Name" },
       { data: "Symbol" },
-      // { data: "TransferAmount" },
-      {
-        data: "Address",
-      },
+      { data: "Address" },
       { data: "IsMainToken" },
       { data: "IsScamToken" },
       {
-        data: "Action",
+        data: "Tags",
+        render: function (e) {
+          if (!e) {
+            return null;
+          }
+          const arrayOfStrings = e.split(",");
+
+          const labelElements = arrayOfStrings.map(
+            (string, index) => `<div class="label" key=${index}>${string}</div>`
+          );
+          return labelElements.join("");
+        },
+      },
+      {
+        data: "Id",
         render: function (e) {
           return `
+          
         <div class="eatherIconcontainer">
-           
             <abbr title="View PNL">
-                <div class="statisIcon actionsIcons erthIcon edit-modal edit-icon-div" width="20px" height="20px" type="button">
+                <div class="statisIcon actionsIcons erthIcon elditTokenPageMainleft edit-modal edit-icon-div" width="20px" height="20px" type="button" id="${e}" >
                 <img src="./edit.png" alt="" />
                 </div>
             </abbr>
-        </div> `;
+            <abbr title="View PNL">
+                <div class="statisIcon actionsIcons elditTokenPageMain erthIcon  edit-icon-div" width="20px" height="20px" type="button" id="${e}" >
+                <img src="./viewM.png" alt="" />
+                </div>
+            </abbr>
+        </div> 
+        
+        `;
         },
       },
     ],
@@ -77,16 +100,18 @@ $(document).ready(function () {
         val.search.search = "";
       });
     },
+
     columnDefs: [
-      { targets: 0, orderable: true, searching: true, width: "10%" },
-      { width: "10%", targets: 1 },
-      { width: "10%", targets: 2 },
-      { width: "10%", targets: 3, orderable: false },
-      { width: "10%", targets: 4 },
-      // { width: "10%", targets: 5 },
-      { width: "10%", targets: 5, orderable: false },
-      { width: "10%", targets: 6, orderable: false },
+      { targets: 0, orderable: true, searching: false, width: "10%" },
+      { width: "10%", targets: 1, searching: false },
+      { width: "10%", targets: 2, searching: false },
+      { width: "10%", targets: 3, orderable: false, searching: false },
+      { width: "10%", targets: 4, searching: false },
+      { width: "10%", targets: 5, orderable: false, searching: false },
+      { width: "10%", targets: 6, orderable: false, searching: true },
+      { width: "10%", targets: 7, orderable: false, searching: true },
     ],
+
     language: {
       lengthMenu: "_MENU_",
     },
@@ -103,7 +128,7 @@ $(document).ready(function () {
         .columns()
         .eq(0)
         .each(function (colIdx) {
-          if (colIdx != 6) {
+          if (colIdx != 7) {
             var cell = $(".filters th").eq(
               $(api.column(colIdx).header()).index()
             );
@@ -113,6 +138,8 @@ $(document).ready(function () {
         });
     },
   });
+
+  table.buttons(".buttons-copy").remove();
   table.button().add(1, {
     action: function (e, dt, button, config) {
       var tableid = button.attr("aria-controls");
@@ -134,8 +161,21 @@ $(document).ready(function () {
   });
 
   var editPopup_bach = $("#edit-trade-modal .modal-content").html();
-
-  $("body").on("click", ".edit-modal", function () {
+  var requestObject = {};
+  $("body").on("click", ".edit-modal", function (e) {
+    $.ajax({
+      url:
+        "http://144.76.163.123/dashboard/tokens.php?action=get_token_detail&token_id=" +
+        e.currentTarget.id,
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        requestObject = data;
+      },
+      error: function (xhr, status, error) {
+        alert("Something Went Wrong");
+      },
+    });
     var id = $(this).closest("tr").find("td:first").text();
 
     $("#edit-trade-modal .modal-content").html(editPopup_bach);
@@ -144,6 +184,13 @@ $(document).ready(function () {
     $(".js-example-tags").select2({
       tags: true,
     });
+
+    $(".js-example-basic-single").select2({
+      minimumResultsForSearch: Infinity,
+    });
+
+    $(".popup-tokenId").html(e.currentTarget.id);
+    $(".updatedStatus").attr("id", e.currentTarget.id);
 
     $.ajax({
       url:
@@ -168,7 +215,7 @@ $(document).ready(function () {
         select.val(allOptions).trigger("change");
       },
       error: function (xhr, status, error) {
-        alert(error);
+        alert("Something Went Wrong");
       },
     });
 
@@ -184,16 +231,74 @@ $(document).ready(function () {
         data: $.param({ tags: jsonData }),
         dataType: "json",
         success: function (data) {
-          alert(data);
+          alert("successful");
         },
         error: function (xhr, status, error) {
-          alert(error);
+          alert("Something Went Wrong");
         },
       });
     });
   });
 
-  $("body").on("click", ".edit-trade-modal .cross-svg", function (e) {
+  $("body").on("click", ".elditTokenPageMain", function (e) {
+    var table = $("#token_table").DataTable();
+
+    $("#token_table tbody").on("click", "tr", function () {
+      var rowData = table.row(this).data();
+      if (rowData) {
+        var addressHTML = rowData.Address;
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(addressHTML, "text/html");
+        var aTag = doc.querySelector("a");
+
+        if (aTag) {
+          var href = aTag.getAttribute("href");
+          var hexCode = href.split("/").pop();
+
+          window.open(
+            "http://144.76.163.123/dashboard/tokeninfo.php?tk=" + hexCode,
+            "_blank"
+          );
+        }
+      }
+    });
+  });
+
+  $("body").on("click", ".updatedStatus", function (e) {
+    var mainTokenValue = $(".main-token");
+    if (mainTokenValue.val()) {
+      requestObject[mainTokenValue.attr("id")] = mainTokenValue.val();
+    }
+    var scamTokenValue = $(".scam-token");
+    if (scamTokenValue.val()) {
+      requestObject[scamTokenValue.attr("id")] = scamTokenValue.val();
+    }
+
+    if (
+      requestObject.hasOwnProperty("IsScamToken") ||
+      requestObject.hasOwnProperty("IsMainToken")
+    ) {
+      $.ajax({
+        url:
+          "http://144.76.163.123/dashboard/tokens.php?action=update_token&token_id=" +
+          e.currentTarget.id,
+        method: "POST",
+        // data: JSON.stringify(requestObject),
+        data: requestObject,
+        success: function (response) {
+          // Handle success response
+          console.log("Data updated successfully");
+        },
+        error: function (xhr, status, error) {
+          // Handle error response
+          console.error("Error updating data:", error);
+        },
+      });
+    }
+    console.log(requestObject, "requestObject->>");
+  });
+
+  $("body").on("click", ".edit-trade-modal .remove-modal", function (e) {
     $("body").removeClass("oflowhidden");
     $(".edit-trade-modal").removeClass("show");
   });
